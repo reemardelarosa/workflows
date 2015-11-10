3c81214c61be7bca75bcc72a03aed28a3c1a53fa
reemardelarosa <jrddelarosa@gmail.com>
2015-11-10 08:44:06 +0000

    New CoffeScript

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require("gulp"),
-    gutil = require("gulp-util") ;
+    gutil = require("gulp-util"),
+    coffee = require("gulp-coffee");
+    
+var coffeeCources = ['components/coffee/tagline.coffee'];
 
-gulp.task('log', function(){
-    gutil.log('Workflows are awesome!');
+gulp.task('coffee', function(){
+    gulp.src(coffeeCources)
+    .pipe(coffee({bare:true})
+        .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'));
 });
